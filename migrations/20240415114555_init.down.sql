cf1028b25da2cf434398adc19d09c7e7d92b05f1
gautam8404 <gautamdhingra8404@gmail.com>
2024-04-16 01:15:11 +0530

    added migrations and other files that were not added

---
+++ migrations/20240415114555_init.down.sql
@@ -0,0 +1,8 @@
+-- Add down migration script here
+DROP INDEX IF EXISTS term_idx;
+DROP INDEX IF EXISTS idx_url;
+DROP TABLE IF EXISTS inverted_index;
+DROP TABLE IF EXISTS terms;
+DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS crawler_queue;
+DROP TABLE IF EXISTS document;
